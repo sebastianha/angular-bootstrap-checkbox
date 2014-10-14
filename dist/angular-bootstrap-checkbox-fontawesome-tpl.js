c58855a621657ca1e482136bd870f218e1454ce7
Joris de Wit <joris.w.dewit@gmail.com>
2014-10-14 07:46:29 -0700

    add ui-checkbox class to template

--- dist/angular-bootstrap-checkbox-fontawesome-tpl.js
+++ dist/angular-bootstrap-checkbox-fontawesome-tpl.js
@@ -2,9 +2,9 @@ angular.module('ui.checkbox').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('angular-bootstrap-checkbox-tpl.html',
-    "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default\" ng-class=\"{'btn-xs': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest'}\">\n" +
+    "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default ui-checkbox\" ng-class=\"{'btn-xs': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest'}\">\n" +
     "  <i ng-style=\"stylefaicon\" class=\"fa\" ng-class=\"{'fa-check': checked===true}\"></i>\n" +
-    "</button>"
+    "</button>\n"
   );
 
 }]);
