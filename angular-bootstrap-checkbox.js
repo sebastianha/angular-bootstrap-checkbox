1b361c6c767c489240550893bc31a500e1f86f97
Dan Shreim <dan@snapjay.com>
2014-10-28 16:37:34 -0400

    Move template to external file

--- angular-bootstrap-checkbox.js
+++ angular-bootstrap-checkbox.js
@@ -6,9 +6,7 @@ angular.module("ui.checkbox", []).directive("checkbox", function() {
 		require: "ngModel",
 		restrict: "E",
 		replace: "true",
-		template: "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default\" ng-class=\"{'btn-xs': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest'}\">" +
-			"<span ng-style=\"styleicon\" class=\"glyphicon\" ng-class=\"{'glyphicon-ok': checked===true}\"></span>" +
-			"</button>",
+		templateUrl: 'template/angular-bootstrap-checkbox/checkbox.html',
 		link: function(scope, elem, attrs, modelCtrl) {
 			scope.size = "default";
 			// Default Button Styling
