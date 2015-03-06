a19e9a6202df3f49d0ac9a5439dcd9cfdda92c3c
Trevor Green <trevor@agreenweb.com>
2015-03-06 12:08:53 -0800

    Adding checked class to outer container when selected.
    There appeared to be no class outputting on the outer container. the
    <button> so if you wanted to style that differently when selected it was
    not possible.

--- angular-bootstrap-checkbox.js
+++ angular-bootstrap-checkbox.js
@@ -6,7 +6,7 @@ angular.module("ui.checkbox", []).directive("checkbox", function() {
 		require: "ngModel",
 		restrict: "E",
 		replace: "true",
-		template: "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default\" ng-class=\"{'btn-xs': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest'}\">" +
+		template: "<button type=\"button\" ng-style=\"stylebtn\" class=\"btn btn-default\" ng-class=\"{'btn-xs': size==='default', 'btn-sm': size==='large', 'btn-lg': size==='largest', 'checked': checked===true}\">" +
 			"<span ng-style=\"styleicon\" class=\"glyphicon\" ng-class=\"{'glyphicon-ok': checked===true}\"></span>" +
 			"</button>",
 		link: function(scope, elem, attrs, modelCtrl) {
